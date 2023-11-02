5b6cd879b43ebf48d4c3a5c300bc3d4ee9a22389
Darthkoppa91 <nowhereextreme@gmail.com>
2023-11-02 12:04:51 +0100

    firstcommit/future/baby/generator

--- script.js
+++ script.js
@@ -1,30 +1,24 @@
-document.getElementById("boy-button").addEventListener("click", function () {
-    calculateBabyHeight("boy");
+document.querySelector("#boy-button").addEventListener("click", function () {
+  calculateBabyHeight("boy");
 });
-document.getElementById("girl-button").addEventListener("click", function () {
-    calculateBabyHeight("girl");
+document.querySelector("#girl-button").addEventListener("click", function () {
+  calculateBabyHeight("girl");
 });
+
+var father = document.querySelector("#Father-height");
+var mother = document.querySelector("#Mother-height");
+
 // formula za visinu buduceg deteta ide tako sto visinu oca bebe saberemo sa visinom majke bebe i dobijeni zbig podelimo sa 2, zatim na to dodamo 6.5 cm za decake i oduzmemo 6.5cm za devojcice.//
-function calculateBabyHeight(gender){
-    var fatherHeight =190;
-    var motherHeight =169;
+function calculateBabyHeight(gender) {
+  var babyHeight = (parseFloat(father.value) + parseFloat(mother.value)) / 2;
 
-    var babyHeight = (fatherHeight+motherHeight) / 2;
+  if (gender === "boy") {
+    babyHeight += 6.5;
+  }
+  if (gender === "girl") {
+    babyHeight -= 6.5;
+  }
 
-    if(gender === "boy") {
-        return babyHeight+= 6.5;
-        document.getElementById("sex-result").textContent ="Baby:Boy";
-
-    } else if( gender === "girl") {
-        babyHeight -= 6.5;
-        document.getElementById("sex-result").textContent ="Baby: Girl";
-        
-    }
-
-    document.getElementById("height-result").textContent ="Estimated Height: " + babyHeight + "cm" ;
-
-
-
-
+  document.getElementById("height-selector").textContent = babyHeight;
 }
-console.log(calculateBabyHeight(190,177));
+// console.log(calculateBabyHeight(190,177));
